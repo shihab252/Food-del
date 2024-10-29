86e91312718ef2e129c8bf7630799cbea58cba32
shihab uddin khan <shihab@kali.Shihab>
2024-10-30 01:58:07 +0600

    first commit

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -26,6 +26,40 @@ const addFood =  async(req,res)=>{
     }
 }
 
+//all food list
+
+const listFood= async (req,res)=>{
+    try {
+        const foods= await foodModel.find({});
+        res.json({Success:true,data:foods})
+        
+    } catch (error) {
+        console.log(error);
+        res.json({Success:false,message:"Error"});
+        
+    }
+
+}
 
 
-export {addFood}
+//remove food item
+
+const removeFood= async(req,res)=>{
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploades/${food.image}`,()=>{})
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({Success:true,message:"Food Removed"})
+        
+    } catch (error) {
+        console.log(error);
+        res.json({Success:false,message:"Error"})
+        
+    }
+
+}
+
+
+
+export {addFood,listFood,removeFood}
