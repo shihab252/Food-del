a46f22b2e8d6228dfd777d9a490d4299988f6f94
shihab uddin khan <shihab@kali.Shihab>
2024-10-31 00:59:20 +0600

    first commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from'./components/Navbar/Navbar'
-import sidebar from './components/sidebar/sidebar'
+import Sidebar from './components/sidebar/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import Order from './pages/Orders/Order'
 
 const App = () => {
   return (
@@ -8,7 +12,12 @@ const App = () => {
     <Navbar/>
       <hr />
       <div className="app-content">
-        <sidebar/>
+      <Sidebar/>
+      <Routes>
+        <Route path='/add' element={<Add/>}/>
+        <Route path='/list' element={<List/>}/>
+        <Route path='/order' element={<Order/>}/>
+      </Routes>
       </div>
 
     </div>
